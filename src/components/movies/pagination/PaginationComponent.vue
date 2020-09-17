46c5f6fe36547f1af65a4f26d8da0d37e2dba073
Nikola <nikolaviki@homail.com>
2020-09-17 14:47:48 +0200

    #PIT4-26 implemented method for fetching prev page

--- src/components/movies/pagination/PaginationComponent.vue
+++ src/components/movies/pagination/PaginationComponent.vue
@@ -3,10 +3,15 @@
     <nav aria-label="...">
       <ul class="pagination justify-content-center">
         <li class="page-item">
-          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          <a
+            class="page-link"
+            tabindex="-1"
+            aria-disabled="true"
+            @click="fetchPreviousPage"
+          >Previous</a>
         </li>
-        <li class="page-item">
-          <a class="page-link" @click="fetchNextPage()">Next</a>
+        <li class="page-item" :class="{ disabled : disableNext}">
+          <a class="page-link" @click="fetchNextPage">Next</a>
         </li>
       </ul>
     </nav>
@@ -26,11 +31,22 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      disableNext: false,
+      disablePrev: true,
+    };
+  },
   methods: {
     fetchNextPage() {
-      if (this.movies.next_page_url) {
-        this.startFetchNextPage(this.movies.current_page + 1);
-      }
+      this.movies.next_page_url
+        ? this.startFetchNextPage(this.movies.current_page + 1)
+        : null;
+    },
+    fetchPreviousPage() {
+      this.movies.prev_page_url
+        ? this.startFetchNextPage(this.movies.current_page - 1)
+        : null;
     },
   },
 };
