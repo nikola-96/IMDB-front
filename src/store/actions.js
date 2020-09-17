af521811e4982bd8fc0ea0d06f4f4ab4bb98c968
Nikola <nikolaviki@homail.com>
2020-09-17 10:34:26 +0200

    #PIT4-33 implemented movies list showing

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,6 +3,7 @@ import movieService from "../services/MovieService";
 export default {
   async startFetchMovies({ commit }) {
     const response = await movieService.fetchAllMovies();
+    console.log(response);
     commit("SET_MOVIES", response.data);
   },
 };
