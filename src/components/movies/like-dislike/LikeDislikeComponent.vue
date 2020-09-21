cf33f0e4b5d97c9465414849e7e428eab8d74e02
Nikola <nikolaviki@homail.com>
2020-09-21 08:14:52 +0200

    #PIT4-4 added like and dislike component

---
+++ src/components/movies/like-dislike/LikeDislikeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <VueLikeDislikeButtons :likes="876547" :dislikes="4567" />
+  </div>
+</template>
+<script>
+import VueLikeDislikeButtons from "vue-like-dislike-buttons";
+import "vue-like-dislike-buttons/src/assets/scss/main.scss";
+
+export default {
+  components: {
+    VueLikeDislikeButtons,
+  },
+};
+</script>
+
+<style></style>
