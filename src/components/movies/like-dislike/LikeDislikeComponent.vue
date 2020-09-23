359e5396fa527c1001e6fe4ae6c287cdb274101d
Nikola <nikolaviki@homail.com>
2020-09-23 18:25:32 +0200

    #PIT4-4 created like dislike component

---
+++ src/components/movies/like-dislike/LikeDislikeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <VueLikeDislikeButtons
+      :likes="movie.likes[0].likes"
+      :dislikes="movie.likes[0].dislikes"
+    />
+  </div>
+</template>
+<script>
+import VueLikeDislikeButtons from "vue-like-dislike-buttons";
+import "vue-like-dislike-buttons/src/assets/scss/main.scss";
+export default {
+  name: "LikeDislikeComponent",
+  components: {
+    VueLikeDislikeButtons,
+  },
+  props: {
+    movie: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/movies/movies-list/SingleMovieComponentForList.vue
+++ src/components/movies/movies-list/SingleMovieComponentForList.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="card mb-3 single" style="max-width: 540px;" @click="redirectToSingleMovie(movie.id)">
+  <div
+    class="card mb-3 single"
+    style="max-width: 540px;"
+    @click="redirectToSingleMovie(movie.id)"
+  >
     <div class="row no-gutters">
       <div class="col-md-4">
         <img :src="movie.image_url" class="card-img" />
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">{{movie.title}}</h5>
-          <p class="card-text">{{movie.description.slice(0, 150)}}...</p>
+          <h5 class="card-title">{{ movie.title }}</h5>
+          <p class="card-text">{{ movie.description.slice(0, 150) }}...</p>
           <p class="card-text"></p>
         </div>
+        <LikeDislikeComponent :movie="movie" />
       </div>
     </div>
   </div>
 </template>
 <script>
+import LikeDislikeComponent from "../like-dislike/LikeDislikeComponent";
 export default {
   name: "SingleMovieComponent",
+  components: {
+    LikeDislikeComponent,
+  },
   props: {
     movie: {
       type: Object,
